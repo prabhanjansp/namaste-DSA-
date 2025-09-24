083a2e021c30a84d41066e451e287679dbda01ff
prabhanjansp <puranikchetan97@gmail.com>
2025-09-24 08:33:27 +0530

    finding the second largest number in the given array with corner cases

--- loop2.js
+++ loop2.js
@@ -1,4 +1,3 @@
-
 // WAF to search an element in an array and return its index if found else return -1
 function searchElement(arr, x) {
   for (let i = 0; i < arr.length; i++) {
@@ -12,35 +11,29 @@ let result = searchElement([1, 2, 3, 4, 5, 6, 7, 8, 9], 2);
 console.log(result);
 // WAF to count the negative numbers in an array
 function countNegativeNumbers(arr) {
-    let count=0
-    for(let i=0;i<arr.length;i++)
-    {
-        if(arr[i]<0)
-        {
-            count++;
-        }
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      count++;
     }
-    return count;
-
+  }
+  return count;
 }
-let result1=countNegativeNumbers([1,-2,3,-4,5,-6,-7,8,9]);
+let result1 = countNegativeNumbers([1, -2, 3, -4, 5, -6, -7, 8, 9]);
 console.log(result1);
-
 
 //find the largest element in the array
 function findLargestNumber(arr) {
-    // let largest=-1;
-    let largest=-Infinity; //this is the correct way to solve the bug
-    for(let i=0;i<arr.length;i++)
-    {
-        if(arr[i]>largest)
-        {
-            largest=arr[i]
-        }
+  // let largest=-1;
+  let largest = -Infinity; //this is the correct way to solve the bug
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
     }
-    return largest;
+  }
+  return largest;
 }
-let result2=findLargestNumber([1,2,3,4,5,6,7,8,9]);
+let result2 = findLargestNumber([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 console.log(result2);
 // largest =-1 is buggy part to solve there are two ways
 // 1) largest=-Infinity
@@ -48,19 +41,36 @@ console.log(result2);
 
 //WAF to find the smallest element in the array
 function findSmallestNumber(arr) {
-    let smallest=Infinity;  //this is the correct way to solve the bug
-    // let smallest=99999; //this is a buggy part
-    for(let i=0;i<arr.length;i++)
-    {
-        if(arr[i]<smallest)
-        {
-            smallest=arr[i]
-        }
+  let smallest = Infinity; //this is the correct way to solve the bug
+  // let smallest=99999; //this is a buggy part
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
     }
-    return smallest;
-}   
-let result3=findSmallestNumber([1,2,3,4,5,6,7,8,-9]);
-console.log(result3);   
-//WAF to find the second largest element in the  array 
-let arr=[4,9,0,2,8,7,15,6,3]
+  }
+  return smallest;
+}
+let result3 = findSmallestNumber([1, 2, 3, 4, 5, 6, 7, 8, -9]);
+console.log(result3);
+//WAF to find the second largest element in the  array
+// let arr=[4,9,0,2,8,7,15,6,3]
 
+function secondLargestNumber(arr) {
+    if(arr.length < 2) {
+        return null; // Not enough elements for second largest
+    }
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] != largest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+
+let output = secondLargestNumber([4]);
+console.log(output);
